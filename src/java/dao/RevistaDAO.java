1d28e443849508fed1898d5d165cacfe5b6ea4b3
Breno Almeida <breno_17@live.com>
2016-06-04 15:18:49 -0300

    revista dao added

---
+++ src/java/dao/RevistaDAO.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Revista;
+import persistencia.DatabaseLocator;
+
+/**
+ *
+ * @author breno
+ */
+public class RevistaDAO implements DAO<Revista>{
+
+    @Override
+    public List<Revista> getAll() {
+        Statement stmt;
+        try {
+            stmt = DatabaseLocator.getConnection().createStatement();
+            String sql = "SELECT * FROM ((item_revista INNER JOIN "
+                    + " item on item_revista.idrevista = item.iditem)";
+            ResultSet rs = stmt.executeQuery(sql);
+            List<Revista> list = new ArrayList<Revista>();
+            while(rs.next()){
+                int idRevista  = rs.getInt("idrevista");;
+                int numero = rs.getInt("numero");
+                String titulo = rs.getString("titulo");
+                Revista revista = new Revista(idRevista, titulo, numero);
+                list.add(revista);
+            }
+            return list;
+        } catch (SQLException ex) {
+            Logger.getLogger(AlunoGraduacaoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AlunoGraduacaoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Revista get(int id) {
+        for(Revista revista : getAll()){
+            if(revista.getId() == id){
+                return revista;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Revista get(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void insert(Revista t) {
+        PreparedStatement stmt;
+        try {
+            //inserir item
+            String sql = "INSERT INTO item (iditem, titulo) "
+                    + "values (?,?)";
+            stmt = DatabaseLocator.getConnection().prepareStatement(sql);
+            stmt.setInt(1, t.getId());
+            stmt.setString(2, t.getTitulo());
+            stmt.execute();
+            stmt.close();
+            //inserir revista
+            sql = "INSERT INTO item_revista (idrevista, numero) "
+                    + "values (?,?)";
+            stmt = DatabaseLocator.getConnection().prepareStatement(sql);
+            stmt.setInt(1, t.getId());
+            stmt.setInt(2, t.getNumero());
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(AlunoGraduacaoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AlunoGraduacaoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void update(Revista t) {
+        try {
+            //update item
+            String sql = "update item set titulo=? where iditem=?";
+            PreparedStatement stmt = DatabaseLocator.getConnection().prepareStatement(sql);
+            stmt.setString(1, t.getTitulo());
+            stmt.setInt(2, t.getId());
+            stmt.execute();
+            stmt.close();
+            //update revista
+            sql = "update item_revista set numero=? where idrevista=?";
+            stmt = DatabaseLocator.getConnection().prepareStatement(sql);
+            stmt.setInt(1, t.getNumero());
+            stmt.setInt(2, t.getId());
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AlunoGraduacaoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void delete(Revista t) {
+        //ItemDAO.getInstance().delete(t);
+    }
+    
+}
